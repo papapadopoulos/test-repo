08f6c9ca4e9638ab3614f5821d960af6d5b0dc6a
panos <p.a.papadopoulos@accenture.com>
2018-11-02 20:53:09 +0200

    Updates

---
+++ precache-manifest.87658927c5aa35dbbc7425eab919be4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-repo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3c3d1d51ff9c0f5f5d4b",
+    "url": "/test-repo/static/js/runtime~main.3c3d1d51.js"
+  },
+  {
+    "revision": "540c5fd88346c7998e63",
+    "url": "/test-repo/static/js/main.540c5fd8.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/test-repo/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "540c5fd88346c7998e63",
+    "url": "/test-repo/static/css/main.20366d3a.chunk.css"
+  },
+  {
+    "revision": "559ebe6a17a2e8225a9003cf7200115d",
+    "url": "/test-repo/index.html"
+  }
+];
